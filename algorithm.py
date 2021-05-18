7d8d85cc2b3395abe5cb7a36841539a9c5075104
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-05-18 15:18:30 +0200

    Add files via upload

--- algorithm.py
+++ algorithm.py
@@ -5,6 +5,12 @@ import math
 
 
 def biased_random_selection (lst, beta):
+    """
+    Given an iterable and a parameter for the quasi-geometric function,
+    this generator operates a biased randomised selection yielding the 
+    elements of the iterable.
+    
+    """
     L = len(lst); options = list(lst)
     for _ in range(L):
         idx = int(math.log(random.random(), 1 - beta)) % len(options)
