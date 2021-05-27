5424b91865de8b6ab6d0c7c2ebdde81bb145af76
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-05-27 12:14:13 +0200

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -10,6 +10,10 @@ def biased_random_selection (lst, beta):
     this generator operates a biased randomised selection yielding the 
     elements of the iterable.
     
+    :param lst: The original iterable (not modified by this method).
+    :param beta: The parameter of the quasi-geometric function.
+    :yield: The selected element with no repetitions.
+    
     """
     L = len(lst); options = list(lst)
     for _ in range(L):
