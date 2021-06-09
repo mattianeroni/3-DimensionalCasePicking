14587cd031f4ba185f192dacfa4647a8cb7d6f35
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-09 11:09:36 +0200

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -37,18 +37,24 @@ locations_per_longaisle = LOCATIONS * (CROSS_AISLES + 1) + CROSS_AISLES + 2
 
 # Populate the graph
 node_id = 0
+pos = dict()
+current_pos = [0, 0]
 for x in range(AISLES):
+    current_pos[1] = 0
     for y in range(locations_per_longaisle):
         G.add_node(node_id)
+        pos[node_id] = tuple(pos)
         if y > 0:
             weight = LOCATION_Y/2 + CROSS_AISLE_SIZE/2 if y in cross_points[1:-1] or (y - 1) in cross_points[1:-1] else LOCATION_Y
+            current_pos[1] += weight
             G.add_edge(node_id, node_id - 1, weight=weight)
         if x > 0 and y in cross_points:
             G.add_edge(node_id, node_id - locations_per_longaisle, weight=LOCATION_X*2)
         node_id += 1
+    current_pos[0] += LOCATION_X*2
 
 # Plot the graph
-#nx.draw(G, with_labels=True, font_weight='bold')
+#nx.draw(G, pos=nodes, with_labels=True, font_weight='bold')
 #plt.show()
 
 # Set the distance matrix
