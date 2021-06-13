ff3891703ca895aac268465e0ee6a2b4832e6989
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-13 14:59:43 +0200

    Add files via upload

--- case.py
+++ case.py
@@ -1,61 +1,66 @@
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                                             *  3-Dimensional Case Picking  *
+This file contains the implementation of case class. Each case represents one of the 3-dimensional items to pick and
+place into the pallets.
+
+Author: Mattia Neroni, Ph.D., Eng. (May 2021).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+"""
 import collections
 
 
-OrderLine = collections.namedtuple("OrderLine", "code cases")
-
+# The order line class
+OrderLine = collections.namedtuple("OrderLine", "code cases location")
 
 
 def rotate (case):
     case.sizex, case.sizey = case.sizey, case.sizex
-
-
-
-def froze (case):
-    case.frozen_x, case.frozen_y = case.x, case.y
-    case.frozen_sizex, case.frozen_sizey = case.sizex, case.sizey
-
+    case.rotated = not case.rotated
 
 
 class Case (object):
 
-    def __init__ (self, x, y, sizex, sizey, weight, height):
-        self.x = x
-        self.y = y
+    def __init__(self, sizex, sizey, sizez, weight, strength):
+        self.position = (0,0,0)
         self.sizex = sizex
         self.sizey = sizey
+        self.sizez = sizez
         self.weight = weight
-        self.height = height
+        self.strength = strength
+        self.rotated = False
 
-        self.frozen_x = None
-        self.frozen_y = None
-        self.frozen_sizex = None
-        self.frozen_sizey = None
+    def rotate (self):
+        self.sizex, self.sizey = self.sizey, self.sizex
+        self.rotated = not self.rotated
 
     def __hash__(self):
         return hash(str(self))
 
     def __repr__(self):
-        return f"Case(x={self.x}, y={self.y}, sizex={self.sizex}, sizey={self.sizey}, height={self.height}, weight={self.weight})"
+        return f"Case(x={self.x}, y={self.y}, z={self.z}, sizex={self.sizex}, sizey={self.sizey}, sizez={self.sizez}, weight={self.weight})"
 
     @property
-    def top (self):
+    def back(self):
         return self.y + self.sizey
 
     @property
-    def right (self):
+    def front(self):
+        return self.y
+
+    @property
+    def right(self):
         return self.x + self.sizex
 
     @property
-    def left (self):
+    def left(self):
         return self.x
 
     @property
-    def down (self):
-        return self.y
+    def top (self):
+        return self.z + self.sizez
 
-    def rotate (self):
-        self.sizex, self.sizey = self.sizey, self.sizex
-
-    def froze (self):
-        self.frozen_x, self.frozen_y = self.x, self.y
-        self.frozen_sizex, self.frozen_sizey = self.sizex, self.sizey
+    @property
+    def bottom (self):
+        return self.z
