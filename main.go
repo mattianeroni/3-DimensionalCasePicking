5075175be20f2bd93a3669fccc4553673b33a2f8
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-15 18:43:59 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
+import (
+	"3DCasePicking/packing"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+
+type P struct {
+	x int
+}
+
+func increase (p *P){
+	p.x++
+}
 
 func main () {
-  
-  
+
+	readfile("./test/testproblem.csv",';')
 }
 
 
 
 
 
-func readfile (filename string) []packing.OrderLine {
+func readfile (filename string, delimiter rune) []packing.OrderLine {
 	orderlines := make([]packing.OrderLine, 0)
 
 	file, err := os.Open(filename)
@@ -18,27 +34,41 @@ func readfile (filename string) []packing.OrderLine {
 		panic(err)
 	}
 
-	r, err2 := csv.NewReader(file).ReadAll()
+	reader := csv.NewReader(file)
+	reader.Comma = delimiter
+	lines, err2 := reader.ReadAll()
 	if err2 != nil {
 		panic(err2)
 	}
 
-	for i, line := range r {
+	for i, line := range lines {
+		fmt.Println(line)
 		if i > 0 {
-			code, _ := strconv.ParseInt(line[0],10,64)
+			code, _ := strconv.ParseInt(line[1],10,64)
 			ncases, _ := strconv.ParseInt(line[2],10,64)
 			sizex, _ := strconv.ParseInt(line[3],10,64)
 			sizey, _ := strconv.ParseInt(line[4],10,64)
-			height, _ := strconv.ParseInt(line[5],10,64)
+			sizez, _ := strconv.ParseInt(line[5],10,64)
 			weight, _ := strconv.ParseInt(line[6],10,64)
+			strength, _ := strconv.ParseInt(line[7],10,64)
+			location, _ := strconv.ParseInt(line[8],10,64)
 
 			cases := make([]packing.Case, ncases)
 			for i := 0; i < int(ncases); i++ {
-				cases[i] = packing.Case{0,0,int(sizex), int(sizey), int(height),int(weight)}
+				cases[i] = packing.Case{
+					X : 0,
+					Y : 0 ,
+					Z : 0,
+					SizeX : int(sizex),
+					SizeY : int(sizey),
+					SizeZ : int(sizez),
+					Weight : int(weight),
+					Strength : int(strength),
+					Rotated : false}
 			}
 
-			orderlines = append(orderlines, packing.OrderLine{int(code), cases})
-			fmt.Println(line)
+			orderlines = append(orderlines, packing.NewOrderLine(byte(code), int(location), cases))
+			//fmt.Println(line)
 		}
 	}
 
