c4ba8d1411dcb918f7dd7e60edbdc8cd64203713
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-18 16:31:28 +0200

    Add files via upload

--- packing/dubepacker.py
+++ packing/dubepacker.py
@@ -4,6 +4,13 @@ import collections
 from case import rotate
 
 
+# Initialize the parameters of the algorithm
+# The minimum supporting surface to make the packing feasible
+# The minimum number of stable corners to make the packing feasible
+MIN_STABLE_SURFACE = 0.7
+MIN_STABLE_CORNERS = 3
+
+
 # Given an index and the already packed case around which
 # we are supposed to try a placement, this method returns
 # the corresponding position.
@@ -33,18 +40,59 @@ def intersect (iCase, jCase):
 # The method needs to iterate all the list of already packed items to avoid overlaps
 # or intersections.
 # It also verifies the stability of cases, the vertical support, and the strength constraint.
-def fit (currentItem, pallet, pos, packed):
+def fit (currentItem, pallet, packed):
     # Check the pallet borders
     X, Y, Z = pallet.size
     if currentItem.right > X or currentItem.back > Y or currentItem.top > Z:
         return False
 
+    # Initialize the stable surface and the stable corners of the currentItem
+    # In a feasible packing, a case must have 3 out of 4 corners, or,
+    # alternatively, the 70% of its surface lying on a case underneath.
+    stableSurface = 0
+    stableCorners = [False,False,False,False]
+    itemSurface = currentItem.sizex * currentItem.sizey
+
+    # Identify the corners that need to be supported
+    footholds = [
+        currentItem.position,
+        (currentItem.left, currentItem.back),
+        (currentItem.right, currentItem.back),
+        (currentItem.right, currentItem.front)
+    ]
+
     for packedItem in packed:
-        # Check intersection with other already placed cases
+        # Check intersection with other already placed cases.
         if intersect(currentItem, packedItem):
             return False
 
-    return True
+        # Check if the currentItem has physical support...
+        if currentItem.position[2] == 0:
+            # If the currentItem is on the floor and has no intersections
+            # the placement is feasible.
+            stableSurface = itemSurface
+            stableCorners = [True,True,True,True]
+
+        elif currentItem.position[2] == packedItem.top:
+            # Update the supported surface.
+            x1 = min(currentItem.right, packedItem.right); x2 = max(currentItem.left, packedItem.left)
+            y1 = min(currentItem.back, packedItem.back); y2 = max(currentItem.front, packedItem.front)
+            if x1 > x2 and y1 > y2:
+                stableSurface += (x1 - x2) * (y1 - y2)
+                # Verify if the vertical support in the corners is provided.
+                for idx, point in enumerate(footholds):
+                    if not stableCorners[idx] and x2 <= point[0] <= x1 and y2 <= point[1] <= y1:
+                        stableCorners[idx] = True
+    # If one of the stability conditions is met, returns a positive response.
+    # The control is made after all the loop because the loop is needed to check
+    # eventual intersections too.
+    if stableSurface / itemSurface > MIN_STABLE_SURFACE or sum(stableCorners) >= MIN_STABLE_CORNERS:
+        return True
+
+    # Arrived at this point, a positive response should have been returned.
+    # If it is not, it mean that there is no intersection between currentItem
+    # and the packed cases, but the vertical support is not provided.
+    return False
 
 
 
@@ -89,12 +137,13 @@ def dubePacker (pallet, cases):
                 # Try with the position
                 pos = getPosition(posIndex, packedItem)
                 currentItem.position = pos
-                if fit(currentItem, pallet, pos, packed):
+                currentItem.level = packedItem.level + 1 if posIndex == 2 else packedItem.level
+                if fit(currentItem, pallet, packed):
                     toPack = False
                     break
                 # Eventually try same position rotating the case
                 rotate(currentItem)
-                if fit(currentItem, pallet, pos, packed):
+                if fit(currentItem, pallet, packed):
                     toPack = False
                     break
 
