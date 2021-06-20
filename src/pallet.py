271277541729b6450f48d91b175734c365d0cb2e
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-06-20 12:11:17 +0200

    Update pallet.py

--- src/pallet.py
+++ src/pallet.py
@@ -1,12 +1,10 @@
-import collections
-
 PALLET_SIZE = (120, 80, 100)
 
 
 class Pallet (object):
     def __init__ (self):
         self.size = PALLET_SIZE
-        self.cases = collections.deque()
+        self.cases = None
 
     def __hash__ (self):
         return str(hash(self))
